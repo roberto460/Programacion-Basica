a9a07b176737bfd852a6a35c83f2513462c04741
roberto460 <romeroroverto265@gmail.com>
2025-02-27 18:55:51 -0600

    Programa de creditos

---
+++ Python/ProgramadeCreditos.py
@@ -0,0 +1,11 @@
+#Programa de creditos
+Creditos=int(input("Ingresar el todal de creditos:"))
+if Creditos >= 80:
+    print("Puede hacer servicio social")
+else:
+    print("No puede hacer servicio social")
+if Creditos >= 120:
+    print("Puede hacer residencias")
+else:
+    print("No puede hacer residencias")
+    
