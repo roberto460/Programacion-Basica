ab9867e4bec9a8ce04d22b1ded4d58b2c2a7673e
roberto460 <romeroroverto265@gmail.com>
2025-03-31 07:52:56 -0600

    31 Marzo 2025

--- Unidad 4/Leerconlibreria.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def leer_diccionarios_de_csv(nombre_archivo):
-
-    with open(nombre_archivo, mode='r', newline='', encoding='utf-8') as archivo:
-        lector = csv.DictReader(archivo)
-        return [fila for fila in lector]
