f4f15dc266acbce9e89d20dd5579b7a12e9fe705
roberto460 <romeroroverto265@gmail.com>
2025-04-09 21:03:44 -0600

    9 abril

--- Unidad 4/Sistemariego.py
+++ Unidad 4/Sistemariego.py
@@ -3,24 +3,27 @@
 import random
 
 def menu_riego():
-    opcion = 0
-    while opcion != 2:
-        print("---- MENÚ ----")
+    opcion = 0  # Definir la opción como entero
+    humedad = 0.0  # Definir la humedad como real
+    
+    while opcion != 2:  # Repetir hasta que opción sea 2
+        print("MENÚ")
         print("1. Medir humedad")
         print("2. Salir")
         opcion = int(input("Elija una opción: "))
         
-        if opcion == 1:
-            humedad = random.randint(10, 100)
+        if opcion == 1:  # Si opción es 1
+            humedad = random.randint(10, 100)  # Generar humedad aleatoria entre 10 y 100
             print(f"Humedad actual: {humedad}%")
-            if humedad < 40:
+            
+            if humedad < 40:  # Si humedad es menor que 40
                 print("Humedad baja. Activando riego...")
-            else:
+            else:  # Sino
                 print("Humedad suficiente. No se activa riego.")
-        elif opcion != 2:
+        
+        elif opcion != 1 and opcion != 2:  # Si opción no es válida
             print("Opción no válida.")
     
-    print("Fin del programa.")
+    print("Fin del programa.")  # Fin del programa
 
 menu_riego()
-        
