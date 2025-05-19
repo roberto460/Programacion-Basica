98147d289f8cdb54eed42a60ad59100d869ab232
roberto460 <romeroroverto265@gmail.com>
2025-05-19 15:34:16 -0600

    19 de mayo

---
+++ Unidad 6/Hackaton/Codigo1.py
@@ -0,0 +1,12 @@
+#Codigo 1.
+
+programa = input("Introduce el nombre del programa:")
+print("Has iniciado el progrma,", programa)
+
+Frase_texto = input("Escribe una frase o texto:")
+
+palabras = Frase_texto.split()
+
+cantidad_palabras = len(palabras)
+print(f"El texto contiene {cantidad_palabras} palabras.")
+
---
+++ Unidad 6/Hackaton/Código2.py
@@ -0,0 +1,25 @@
+#Código 2.
+
+texto = input("Introduce un texto para analizar: ")
+
+contador_vocales = 0
+contador_consonantes = 0
+contador_numeros = 0
+contador_otros = 0
+
+for caracter in texto:
+    caracter = caracter.lower()
+    if caracter in "aeiou":
+        contador_vocales += 1
+    elif caracter.isalpha():  
+        contador_consonantes += 1
+    elif caracter.isdigit(): 
+        contador_numeros += 1
+    else:
+        contador_otros += 1
+
+
+print(f"Vocales: {contador_vocales}")
+print(f"Consonantes: {contador_consonantes}")
+print(f"Números: {contador_numeros}")
+print(f"Otros caracteres: {contador_otros}")
